dffe57f3a4d485237a3dfedc113c34ac30685bbd
mackeesy-cmyk <mackeesy@gmail.com>
2026-05-06 13:06:38 +0200

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode, lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -38,5 +39,6 @@ createRoot(document.getElementById('root')).render(
         } />
       </Routes>
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 )
